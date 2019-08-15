63b3a6ec60fb35a5da0f921dea6fd5d89b12517a
aishimeth2135 <cyteria39@gmail.com>
2019-08-15 13:52:18 +0800

    Fix some bugs of Enchant Simulator

--- src/js/main/EnchantSimulator/module/LanguageData/zh_tw.js
+++ src/js/main/EnchantSimulator/module/LanguageData/zh_tw.js
@@ -12,11 +12,17 @@ const zh_tw = {
         'step title': '步驟',
         'enchant per time: title': '每次附',
         'Original Potential': '初始潛力值',
+        'Base Potential': '基礎潛力值',
         'Success Rate': '成功率',
         'Character Level': '角色等級',
+        'tips': {
+            'Base Potential': '「製作裝備」時，裝備的基礎潛力值。大部分情況下無需設定。'
+        },
         'Warn': {
             'Step Stat Repeat': '單一步驟不可存在重複的能力。',
-            'Number of Equipment Item Exceeding the maximum': '可附魔的能力數量已達上限。',
+            'Number of Equipment Item exceeding the maximum': '可附魔的能力數量已達上限。',
+            'Potential of Equipment has been less than 1': '裝備潛力值已小於1，無法繼續附魔。',
+            'Potential of Step is less than 1': '提示：有部分前置步驟的結算潛力值小於1，此附魔無法實作。',
             'Success to copy': '已複製完畢。'
         }
     }
