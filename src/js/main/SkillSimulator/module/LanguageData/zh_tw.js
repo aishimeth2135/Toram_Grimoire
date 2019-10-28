bdfebcc235d375a7fa61a31cbba1ae449d3abe15
aishimeth2135 <aishimeth2135@gmail.com>
2019-10-28 17:25:04 +0800

    Fix bug of last version

--- src/js/main/SkillSimulator/module/LanguageData/zh_tw.js
+++ src/js/main/SkillSimulator/module/LanguageData/zh_tw.js
@@ -3,6 +3,14 @@ export default function(){
         'Top List': {
             'item 1': '布偶的魔法書',
             'item 2': ''
+        },
+        'Skill Simulator': {
+            'Controller': {
+                'main menu': {
+                    'operating': '操作',
+                    'step value': '數值'
+                }
+            }
         }
     };
 }
