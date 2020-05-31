77129908d9e616f612721de2307006383f6fe858
aishimeth2135 <aishimeth2135@gmail.com>
2020-05-31 23:20:38 +0800

    Fix UI of mobile

--- public/sw.js
+++ public/sw.js
@@ -3,78 +3,78 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 workbox.googleAnalytics.initialize();
 
 workbox.core.setCacheNameDetails({
-    prefix: 'cy',
-    suffix: 'v1'
+  prefix: 'cy',
+  suffix: 'v1'
 });
 
 // image
 workbox.routing.registerRoute(
-    /.*\.(?:png|jpg|jpeg|svg|gif)/,
-    new workbox.strategies.CacheFirst({
-        cacheName: 'image-cache',
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-            })
-        ]
-    })
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+      })
+    ]
+  })
 );
 
 // font
 workbox.routing.registerRoute(
-    /.*\.(?:ttf|woff|woff2)/,
-    new workbox.strategies.CacheFirst({
-        cacheName: 'font-cache',
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 180, // 180 days
-            })
-        ]
-    })
+  /.*\.(?:ttf|woff|woff2)/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'font-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 180, // 180 days
+      })
+    ]
+  })
 );
 
 // google spreadsheets csv
 workbox.routing.registerRoute(
-    /^https:\/\/docs\.google\.com\/spreadsheets\/.+output\=csv.+/,
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'google-spreadsheets-csv-files',
-        plugins: [
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200]
-            })
-        ]
-    })
+  /^https:\/\/docs\.google\.com\/spreadsheets\/.+output\=csv.+/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-spreadsheets-csv-files',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
 );
 
 // iconify icons
 workbox.routing.registerRoute(
-    /^https:\/\/api\.iconify\.design/,
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'iconify-design-icons',
-        plugins: [
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200]
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
-            })
-        ]
-    })
+  /^https:\/\/api\.iconify\.design/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'iconify-design-icons',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+      })
+    ]
+  })
 );
 
 workbox.precaching.precacheAndRoute([
   {
     "url": "dist/grimoire.min.js",
-    "revision": "a2b279b378c7836f2aa7456c65e4e615"
+    "revision": "c31f0b6ec6dfded59024f84f0593987e"
   },
   {
     "url": "index.html",
-    "revision": "b91cd3ae6eb81aa4a524f1bd1a0f19c0"
+    "revision": "b5b1b689a4a0777874daf6d8c15d274d"
   },
   {
     "url": "manifest.json",
     "revision": "b46b362285c754e8d08c068f544adb21"
   }
 ], {
-    ignoreURLParametersMatching: [/source/, /calculation_data/]
+  ignoreURLParametersMatching: [/source/, /calculation_data/]
 });
