06fdf6d0922f88d4806985722c958579b7ece5e8
Cyteria <aishimeth2135@gmail.com>
2020-07-07 19:33:52 +0800

    Fix bug of Skill Query
    - [fix bug] split char of attr#extra_values of branch@heal: "," change to ",,"

--- src/views/SkillQuery/skill-branch/heal-formula.vue
+++ src/views/SkillQuery/skill-branch/heal-formula.vue
@@ -8,13 +8,16 @@
       v-html="showData['constant']">
     </span>
     <cy-icon-text iconify-name="ic-round-add" v-if="showData['constant'] && showData['@extra-value-list'].length != 0" />
-    <span v-for="(data, i) in showData['@extra-value-list']" :key="data.text + data.value"
-      class="extra-value">
-      <span class="attr-scope title">{{ data.text }}</span>
-      <cy-icon-text iconify-name="ic-round-close" />
-      <span class="attr-scope value" v-html="data.value"></span>
-      <cy-icon-text v-if="i != showData['@extra-value-list'].length - 1" iconify-name="ic-round-add" />
-    </span>
+
+    <template v-for="(data, i) in showData['@extra-value-list']">
+      <span class="extra-value" :key="data.text + data.value + '-value'">
+        <span class="attr-scope title">{{ data.text }}</span>
+        <cy-icon-text iconify-name="ic-round-close" />
+        <span class="attr-scope value" v-html="data.value"></span>
+      </span>
+      <cy-icon-text v-if="i != showData['@extra-value-list'].length - 1" iconify-name="ic-round-add"
+        :key="data.text + data.value + '-icon'" />
+    </template>
   </div>
 </template>
 <script>
