57b4836b672e2ddbf20579850c28e220bdfe7d98
Cyteria <aishimeth2135@gmail.com>
2020-07-24 17:25:46 +0800

    Adjust sw.js

--- src/sw.js
+++ src/sw.js
@@ -2,21 +2,21 @@ workbox.googleAnalytics.initialize();
 
 workbox.core.setCacheNameDetails({
   prefix: 'cy',
-  suffix: 'v1'
+  suffix: 'v2'
 });
 
-// image
-workbox.routing.registerRoute(
-  /.*\.(?:png|jpg|jpeg|svg|gif)/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-      })
-    ]
-  })
-);
+// // image
+// workbox.routing.registerRoute(
+//   /.*\.(?:png|jpg|jpeg|svg|gif)/,
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'image-cache',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+//       })
+//     ]
+//   })
+// );
 
 // font
 workbox.routing.registerRoute(
@@ -45,33 +45,33 @@ workbox.routing.registerRoute(
 );
 
 // google app script
-workbox.routing.registerRoute(
-  /^https:\/\/script.google.com\/macros\/s\/AKfycbxGeeJVBuTL23gNtaC489L_rr8GoKfaQHONtl2HQuX0B1lCGbEo\/exec?url=.+/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'app-script--get-csv',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      })
-    ]
-  })
-);
+// workbox.routing.registerRoute(
+//   /^https:\/\/script.google.com\/macros\/s\/AKfycbxGeeJVBuTL23gNtaC489L_rr8GoKfaQHONtl2HQuX0B1lCGbEo\/exec?url=.+/,
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: 'app-script--get-csv',
+//     plugins: [
+//       new workbox.cacheableResponse.Plugin({
+//         statuses: [0, 200]
+//       })
+//     ]
+//   })
+// );
 
 // iconify icons
-workbox.routing.registerRoute(
-  /^https:\/\/api\.iconify\.design/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'iconify-design-icons',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      }),
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
-      })
-    ]
-  })
-);
+// workbox.routing.registerRoute(
+//   /^https:\/\/api\.iconify\.design/,
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: 'iconify-design-icons',
+//     plugins: [
+//       new workbox.cacheableResponse.Plugin({
+//         statuses: [0, 200]
+//       }),
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+//       })
+//     ]
+//   })
+// );
 
 workbox.precaching.precacheAndRoute([], {
   ignoreURLParametersMatching: [/source/, /calculation_data/]
