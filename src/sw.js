a4e38465af6b447bb5ae8689d6cb07be67311b3f
aishimeth2135 <aishimeth2135@gmail.com>
2020-07-25 02:02:08 +0800

    Fix bug of precache

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 workbox.googleAnalytics.initialize();
 
 workbox.core.setCacheNameDetails({
-  prefix: 'cy',
+  prefix: 'toram-grimoire',
   suffix: 'v2'
 });
 
@@ -73,6 +73,18 @@ workbox.routing.registerRoute(
 //   })
 // );
 
-workbox.precaching.precacheAndRoute([], {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {
   ignoreURLParametersMatching: [/source/, /calculation_data/]
 });
