2218e75b16ed9899bb90584447920621ba0237cc
Kyrie Chen <kyriechen@qnap.com>
2021-06-16 18:07:23 +0800

    [4.1.10] temp version

--- src/lib/main/utils/math/index.js
+++ src/lib/main/utils/math/index.js
@@ -1,4 +1,12 @@
 function getGcd(a, b) {
+  if (typeof a !== 'number') {
+    a = 1;
+    console.warn('[math: get GCD] param a:', a, 'is not number.');
+  }
+  if (typeof b !== 'number') {
+    b = 1;
+    console.warn('[math: get GCD] param b:', b, 'is not number');
+  }
   const max = Math.max(a, b);
   const min = Math.min(a, b);
   if (max % min === 0) {
