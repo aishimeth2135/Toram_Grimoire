09c33c839321b4a6c10f137a167c564ef4b9366f
aishimeth2135 <aishimeth2135@gmail.com>
2021-07-17 18:11:50 +0800

    [4.1.25] Fix bug of enchant-system

--- src/store/main/index.js
+++ src/store/main/index.js
@@ -2,7 +2,7 @@ const store = {
   namespaced: true,
   state: {
     redirectPath: null,
-    version: '4.1.24',
+    version: '4.1.25',
     serviceWorker: {
       instance: null,
       hasUpdate: false,
