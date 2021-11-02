1531596075918e93dbd09a9473ab4456f4461a24
aishimeth2135 <aishimeth2135@gmail.com>
2021-11-02 19:56:03 +0800

    [4.2.16] Fix bug of handle formula

--- src/store/main/index.js
+++ src/store/main/index.js
@@ -2,7 +2,7 @@ const store = {
   namespaced: true,
   state: {
     redirectPath: null,
-    version: '4.2.14',
+    version: '4.2.16',
     serviceWorker: {
       instance: null,
       hasUpdate: false,
