9ed7d405848af4a32f693e62ac8c909a8ef133cf
aishimeth2135 <aishimeth2135@gmail.com>
2021-12-29 20:53:31 +0800

    [4.3.11] Fix bug of button-check and button-radio

--- src/store/main/index.js
+++ src/store/main/index.js
@@ -4,7 +4,7 @@ const store = {
   namespaced: true,
   state: {
     redirectPathName: null,
-    version: '4.3.10',
+    version: '4.3.11',
     serviceWorker: {
       instance: null,
       hasUpdate: false,
