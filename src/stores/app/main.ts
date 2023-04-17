c15ba8da73495f93f7a537293d5e128fa1fd27de
Cyteria <cyteria39@gmail.com>
2023-04-17 23:50:38 +0800

    fix(enchant-doll): update calc methods

--- src/stores/app/main.ts
+++ src/stores/app/main.ts
@@ -5,7 +5,7 @@ import Grimoire from '@/shared/Grimoire'
 
 import { useLanguageStore } from './language'
 
-const version = '4.6.15'
+const version = '4.6.17'
 
 export const useMainStore = defineStore('app-main', () => {
   const settingVisible = ref(false)
