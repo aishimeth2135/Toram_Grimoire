b2e040ff8596052774e42371a2263ede7d6db387
Cyteria <cyteria39@gmail.com>
2023-06-17 21:11:44 +0800

    feat: update app version

--- src/stores/app/main.ts
+++ src/stores/app/main.ts
@@ -5,7 +5,7 @@ import Grimoire from '@/shared/Grimoire'
 
 import { useLanguageStore } from './language'
 
-const version = '4.6.18'
+const version = '4.6.20'
 
 export const useMainStore = defineStore('app-main', () => {
   const settingVisible = ref(false)
