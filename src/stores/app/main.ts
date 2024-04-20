f2b6d3a10ddf6852a5a390beff12ec6099b04e26
Cyteria <cyteria39@gmail.com>
2024-04-20 09:01:02 +0800

    fix(skill): fix the skill icons

--- src/stores/app/main.ts
+++ src/stores/app/main.ts
@@ -5,7 +5,7 @@ import Grimoire from '@/shared/Grimoire'
 
 import { useLanguageStore } from './language'
 
-const version = '5.0.2'
+const version = '5.0.3'
 
 export const useMainStore = defineStore('app-main', () => {
   const settingVisible = ref(false)
