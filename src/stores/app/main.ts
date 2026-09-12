087cc6c4e8f950e059f41319ad5046784d68e949
Cyteria <cyteria39@gmail.com>
2026-09-12 17:18:35 +0800

    feat: update version number to v5.3.2

--- src/stores/app/main.ts
+++ src/stores/app/main.ts
@@ -5,7 +5,7 @@ import Grimoire from '@/shared/Grimoire'
 
 import { useLocaleStore } from './locale'
 
-const version = '5.3.1'
+const version = '5.3.2'
 
 export const useMainStore = defineStore('app-main', () => {
   type ServiceWorkerUpdateHandler = (reloadPage?: boolean) => Promise<void>
